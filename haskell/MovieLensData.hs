d364975ff5a4ac998ac6fc7bffdc836975cc929d
lukas <lhofmaie@hsr.ch>
2014-11-14 14:17:37 +0100

    zusaetliche datensaetze zu movielens hinzugefuegt

--- haskell/MovieLensData.hs
+++ haskell/MovieLensData.hs
@@ -1,6 +1,14 @@
 module MovieLensData
 (loadData,
-loadMatrix
+loadMatrix,
+ypath,
+xpath,
+thetapath,
+xrpath,
+yrpath,
+thetarpath,
+rpath,
+rrpath
 )
 where
 
@@ -11,27 +19,39 @@ import Data.List.Split as S
 
 main = do
      ts <- loadData filepath 
-     putStrLn ("end of movielens main: " ++ (show (reduced_r (createR ts))))
+     putStrLn "end of movielens main: " 
 
 filepath :: String
 filepath = "/home/lukas/oschena/ml-100k/u1.base"
-ypath = "/home/lukas/oschena/y.txt"
 
-loadData::FilePath -> IO [[Int]]
+yrpath = "/home/lukas/mfbuild/y336x188.txt"
+xrpath = "/home/lukas/mfbuild/x336x188.txt"
+thetarpath = "/home/lukas/mfbuild/theta336x188.txt"
+rrpath = "/home/lukas/mfbuild/r336x188.txt"
+
+ypath = "/home/lukas/mfbuild/y1682x943.txt"
+xpath = "/home/lukas/mfbuild/x1682x943.txt"
+thetapath = "/home/lukas/mfbuild/theta1682x943.txt"
+rpath = "/home/lukas/mfbuild/r1682x943.txt"
+
+loadData::FilePath -> IO [[Float]]
 loadData x = do
          contents <- readFile x
          return $ strs2int $ lines contents
 
-loadMatrix :: FilePath -> IO (Matrix Int)
+loadMatrix :: FilePath -> IO (Matrix Float)
 loadMatrix x = do
   contents <- readFile x
   return $ fromLists (strs2int $ lines contents)
 
-strs2int :: [String] -> [[Int]]
+strs2int :: [String] -> [[Float]]
 strs2int xs = [str2ints x | x <- xs]
 
-str2ints :: String -> [Int]
-str2ints str = [read x | x <- tail (S.splitOn " " str)]
+str2ints :: String -> [Float]
+str2ints str = [read x | x <- strxs str]
+
+strxs :: String -> [String]
+strxs str = tail $ S.splitOn " " str
 
 strs2texts :: [String] -> [[Int]]
 strs2texts strlist = [init $ (texts2ints.str2text) x | x <- strlist ]
@@ -51,14 +71,13 @@ nr_item = 943
 rnr_user = 4
 rnr_item = 5
 
-r :: Matrix Int
 r = zero nr_user nr_item
 
-createR :: [[Int]] -> Matrix Int
-createR xs = foldl addRating r $ take 100 xs
+--createR :: [[Int]] -> Matrix Float
+--createR xs = foldl addRating r $ take 100 xs
 
 addRating :: Matrix Int -> [Int] -> Matrix Int
-addRating acc (u:i:r:xs) = setElem r (u, i) acc
+addRating acc (u:i:r:xs) = setElem (fromIntegral r) (u, i) acc
 
-reduced_r :: Matrix Int -> Matrix Int
+reduced_r :: Matrix Float -> Matrix Float
 reduced_r x = submatrix 1 rnr_item 1 rnr_user x
