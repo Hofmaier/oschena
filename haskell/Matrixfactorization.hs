da422ff50865bb1e059a8be206ea90ac72fcd2fc
lukas <lhofmaie@hsr.ch>
2014-11-14 14:02:29 +0100

    reimplement cost. it was too slow too memory hungry

--- haskell/Matrixfactorization.hs
+++ haskell/Matrixfactorization.hs
@@ -1,5 +1,12 @@
 module Matrixfactorization
 (
+cost,
+cost2,
+costtest,
+gradientdescent,
+ratings,
+latentfactors,
+y2r
 )
 where
 
@@ -12,7 +19,7 @@ type Parameter = (Matrix Float, Matrix Float)
 type Ratings = Matrix Float
 type CostFunc = Parameter -> (Gradient, Float)
 
-iter = 100
+iter = 10
 
 alpha = 0.01
 
@@ -35,11 +42,13 @@ userlatentfactors = [0.28544, -1.68427, 0.26294, 0.50501, -0.45465, 0.31746, -0.
 userlfmat :: M.Matrix Float
 userlfmat = M.fromList rnr_user rn_features userlatentfactors
 
+initialparameter = (itemlfmat, userlfmat)
+
 ymat :: Matrix Float
 ymat = M.fromList rnr_item rnr_user [5, 4, 0, 0, 3, 0, 0, 0, 4, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0]
 
 j :: Float
-j =  costvalue (y2r ymat) (sqerr (err itemlfmat userlfmat ymat)) (reg itemlfmat userlfmat)
+j =  costvalue (y2r ymat) (sqerr (err itemlfmat userlfmat ymat)) (reg itemlfmat)
 
 o :: Matrix Float
 o = piecewisem (y2r ymat) (sqerr (err itemlfmat userlfmat ymat))
@@ -65,8 +74,8 @@ summ m = Prelude.sum [V.sum (M.getRow x m) | x <-[1 .. M.nrows m]]
 summv :: Float
 summv = summ piecewisemv
 
-reg :: Matrix Float -> Matrix Float -> Float
-reg x theta =  (lambda / 2) * ((Prelude.sum $ sumsq x) + (Prelude.sum $ sumsq theta))
+reg :: Matrix Float -> Float
+reg m =  Prelude.sum $ sumsq m
 
 err :: Matrix Float -> Matrix Float -> Matrix Float -> Matrix Float
 err x theta y = (M.multStd x $ M.transpose theta) - y
@@ -80,28 +89,58 @@ piecewisem v u = matrix (M.nrows v) (M.ncols u) (\(i, j) -> (M.getElem i j v) * 
 costvalue :: Matrix Float -> Matrix Float -> Float -> Float
 costvalue r m reg =  ((summ (piecewisem r m)) / 2.0) + reg
 
-gradientdescent :: Parameter -> Ratings -> Parameter
-gradientdescent p y = Prelude.foldl (gradientstep y)  p [1 .. 100]
+gradientdescent :: Parameter -> Ratings -> Matrix Float -> Parameter
+gradientdescent p y r = Prelude.foldl (gradientstep y r)  p [1 .. iter]
 
-gradientstep :: Ratings -> Parameter -> Int -> Parameter
-gradientstep y (xp, thetap) i = (subgrad xp thetap y, subgrad thetap xp y)
+gradientstep :: Ratings -> Matrix Float -> Parameter -> Int -> Parameter
+gradientstep y r (xp, thetap) i = (subgrad xp thetap y r, subgrady (xp, thetap) y r)
 
-subgrad :: Matrix Float -> Matrix Float -> Ratings -> Matrix Float
-subgrad x theta y = x - (M.scaleMatrix alpha (xgrad (x,theta) y))
+subgrad :: Matrix Float -> Matrix Float -> Ratings -> Matrix Float -> Matrix Float
+subgrad x theta y r = x - (M.scaleMatrix alpha (xgrad (x,theta) y r))
+
+subgrady :: Parameter -> Ratings -> Matrix Float -> Gradient 
+subgrady (x, theta) y r = theta - (M.scaleMatrix alpha (ygrad (x, theta) y r))
 
 costfunc :: Parameter -> Ratings -> (Gradient, Float)
 costfunc p y = (y, 100.0)
 
-cost :: (Matrix Float -> Matrix Float -> Matrix Float -> (Matrix Float, Matrix Float, Float))
-cost x theta y = (x, x, costvalue (y2r y) (sqerr (err x theta y)) (reg x theta))
+cost :: Parameter -> Ratings -> Float
+cost (x, theta) y = costvalue (y2r y) (sqerr (err x theta y)) (reg x)
 
-xgrad :: Parameter -> Ratings -> Gradient
-xgrad (x, theta) y = (M.multStd (piecewisem e r) theta) + M.scaleMatrix lambda x
-                  where r = y2r y
-                        e = err x theta y
+cost2 :: Parameter -> Ratings -> Float
+cost2 (x, theta) y =  (error2/2 ) + regularization
+  where error2 = Prelude.sum $ [ rdiff p r | (p, r) <- knownratings (x, theta) y]
+        regularization = (lambda / 2) * ((reg x) + (reg theta))
 
+error3 :: Parameter -> Ratings -> Float
+error3 (x,theta) y = Prelude.sum $ [ rdiff p r | (p, r) <- knownratings (x, theta) y]
 
+error3v = error3 latentfactors ymat
 
+rdiff :: Float -> Float -> Float
+rdiff p r = (r - p)^2
+
+knownratings :: Parameter -> Ratings -> [(Float, Float)]
+knownratings p r = Prelude.filter (\(p, r) -> r /= 0.0) $ yrzip p r
+
+yrzip :: Parameter -> Ratings -> [(Float, Float)]
+yrzip (x, theta) y = Prelude.zip (M.toList (M.multStd x $ M.transpose theta)) $ M.toList y
+
+xgrad :: Parameter -> Ratings -> Gradient -> Matrix Float
+xgrad (x, theta) y r = (M.multStd (piecewisem e r) theta) + M.scaleMatrix lambda x
+  where e = err x theta y
+
+ygrad :: Parameter -> Ratings -> Matrix Float -> Gradient 
+ygrad (x, theta) y r = (M.multStd (M.transpose (piecewisem e r)) x) + M.scaleMatrix lambda theta
+  where e = err x theta y
+
+main = do
+  y <- Ml.loadMatrix Ml.yrpath
+  x <- Ml.loadMatrix Ml.xrpath
+  theta <- Ml.loadMatrix Ml.thetarpath
+  r <- Ml.loadMatrix Ml.rpath
+  putStrLn $ "run gradient descent: " Prelude.++ (show $ gradientdescent (x,theta) y r)
+            
 sqerrv :: Matrix Float
 sqerrv = sqerr errv
 
@@ -112,10 +151,21 @@ piecewisemv :: Matrix Float
 piecewisemv = piecewisem sqerrv $ y2r ymat
 
 regv :: Float
-regv = reg itemlfmat userlfmat
+regv = reg itemlfmat
+
+reg2v :: Float
+reg2v = (lambda / 2) * ((reg itemlf) + (reg userlf))
 
 sqerrm :: Matrix Float
 sqerrm = M.fromList 2 2 [1,2,3,4]
 
 msquare :: Vector Float -> Vector Float
 msquare xs = V.map (\x -> x^2) xs
+
+ratings = ymat
+itemlf = itemlfmat
+userlf = userlfmat
+latentfactors = (itemlf, userlf)
+errorfunc = cost
+costtest = cost latentfactors ratings
+
